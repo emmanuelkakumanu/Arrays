e35cc538cf0d09c7776595fd7556e186f0e03f63
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-12-14 11:41:37 +0530

    Create 2.java

---
+++ 2.java
@@ -0,0 +1,29 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+			a[i]=s.nextInt();
+		Ideone ob=new Ideone();
+		ob.add(n,a);
+	}
+	void add(int m,int[] b)
+	{
+		int sum=0;
+		for(int i=0;i<m;i++)
+		{
+			sum=sum+b[i];
+		}
+		System.out.print("Average : "+(sum/m));
+	}
+}
